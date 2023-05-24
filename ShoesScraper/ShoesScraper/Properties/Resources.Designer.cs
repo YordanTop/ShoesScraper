f6a911915925bdebaa64e3e4a87dcc61843d8f72
YordanTop <119804802+YordanTop@users.noreply.github.com>
2023-05-24 08:44:09 -0700

    Add files via upload

--- ShoesScraper/ShoesScraper/Properties/Resources.Designer.cs
+++ ShoesScraper/ShoesScraper/Properties/Resources.Designer.cs
@@ -60,15 +60,6 @@ namespace ShoesScraper.Properties {
             }
         }
         
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap back_button__1_ {
-            get {
-                object obj = ResourceManager.GetObject("back-button (1)", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
         
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
